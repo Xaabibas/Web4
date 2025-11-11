46ae7b8291d2b7db863937e08d40024819cf68ef
Xaabibas <tumuyhabirov@yandex.ru>
2025-11-11 22:14:34 +0300

    change Checker

--- src/main/java/web4/model/Checker.java
+++ src/main/java/web4/model/Checker.java
@@ -12,26 +12,26 @@ public class Checker {
 
     public boolean checkBoxWithPositiveR(double x, double y, double r) {
         if (x >= 0 && y >= 0) {
-            return x*x + y*y <= r*r;
+            return x*x + y*y <= r*r / 4;
         }
         if (x >= 0 && y < 0) {
-            return y >= 2 * x - r;
+            return x <= r/2 && y >= -r;
         }
-        if (x < 0 && y <= 0) {
-            return x >= -r && y >= -r;
+        if (x < 0 && y >= 0) {
+            return y <= x + r;
         }
         return false;
     }
 
     public boolean checkBoxWithNegativeR(double x, double y, double r) {
         if (x <= 0 && y <= 0) {
-            return x*x + y*y <= r*r;
+            return x*x + y*y <= r*r / 4;
         }
         if (x <= 0 && y > 0) {
-            return y <= 2 * x - r;
+            return x >= -r / 2 && y <= -r;
         }
-        if (x > 0 && y >= 0) {
-            return x <= -r && y <= -r;
+        if (x > 0 && y <= 0) {
+            return y >= x + r;
         }
         return false;
     }
